e892b5aba3e1a5fce8ce4f35349ce27972a637ce
chowie <christopher.howie@gmail.com>
2023-06-08 17:36:20 +0100

    Combined choropleth map and bootstrap histogram example

--- app.py
+++ app.py
@@ -1,49 +1,61 @@
-# Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+
+
+from dash import Dash, dcc, html, Input, Output
 import pandas as pd
 import plotly.express as px
 import dash_bootstrap_components as dbc
-
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
 # Initialize the app - incorporate a Dash Bootstrap theme
 external_stylesheets = [dbc.themes.CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
+df = px.data.election()  # replace with your own data source
+
 # App layout
 app.layout = dbc.Container([
     dbc.Row([
-        html.Div('My First App with Data, Graph, and Controls', className="text-primary text-center fs-3")
+        html.Div('Montreal 2013 candidate voting pool analysis',
+                 className="text-primary text-center fs-3")
     ]),
 
     dbc.Row([
-        dbc.RadioItems(options=[{"label": x, "value": x} for x in ['pop', 'lifeExp', 'gdpPercap']],
-                       value='lifeExp',
-                       inline=True,
-                       id='radio-buttons-final')
+        html.P("Select a candidate:"),
+        dcc.RadioItems(
+            id='candidate',
+            options=["Joly", "Coderre", "Bergeron"],
+            value="Coderre",
+            inline=True
+        ),
+
     ]),
 
     dbc.Row([
         dbc.Col([
-            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
+            dcc.Graph(figure=px.histogram(
+                df, x='winner', y='total', histfunc='sum'), id='sum-histogram')
         ], width=6),
 
         dbc.Col([
-            dcc.Graph(figure={}, id='my-first-graph-final')
+            dcc.Graph(id="graph"),
         ], width=6),
     ]),
 
 ], fluid=True)
 
-# Add controls to build the interaction
-@callback(
-    Output(component_id='my-first-graph-final', component_property='figure'),
-    Input(component_id='radio-buttons-final', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+
+@app.callback(
+    Output("graph", "figure"),
+    Input("candidate", "value"))
+def display_choropleth(candidate):
+    geojson = px.data.election_geojson()
+    fig = px.choropleth(
+        df, geojson=geojson, color=candidate,
+        locations="district", featureidkey="properties.district",
+        projection="mercator", range_color=[0, 6500])
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     return fig
+
 
 # Run the app
 if __name__ == '__main__':
